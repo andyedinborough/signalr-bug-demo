6d831e395bbf3de5e69792cebcf1acf8468fe8b8
Andy Edinborough <andy@edinborough.org>
2013-02-20 22:40:36 -0600

    more simplifying

--- signalr-webapp/Global.asax.cs
+++ signalr-webapp/Global.asax.cs
@@ -1,9 +1,12 @@
-﻿using System.Web.Routing;
+﻿using signalr_webapp.Lib;
+using System.Web.Routing;
 
 namespace signalr_webapp {
 	public class MvcApplication : System.Web.HttpApplication {
 		protected void Application_Start() {
-			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			RouteTable.Routes.MapConnection<MyConnection>("Echo", "echo", new Microsoft.AspNet.SignalR.ConnectionConfiguration {
+				EnableCrossDomain = true
+			});
 
 			AuthenticateRequest += MvcApplication_AuthenticateRequest;
 		}
